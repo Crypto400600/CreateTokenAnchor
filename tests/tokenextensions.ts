96614f489ebabc5e6742364999fc983214216a33
ether-genius <luisd.trivino@gmail.com>
2024-08-22 10:29:36 -0700

    burntoken test

--- tests/tokenextensions.ts
+++ tests/tokenextensions.ts
@@ -175,5 +175,37 @@ describe("tokenextensions", () => {
       console.log(err);
     }
   });
+  it("should burn a token of payer wallet ", async () => {
+    try {
+      const [splTokenMint, _1] = await findSplTokenMintAddress();
+
+      const [vaultMint, _2] = await findVaultAddress();
+
+      const [payerMintAta, _3] = await findAssociatedTokenAccountAddress(
+        payer.publicKey,
+        splTokenMint
+      );
+
+      const tx = await program.methods
+        .burnToken()
+        .accounts({
+          splTokenMint: splTokenMint,
+          vault: vaultMint,
+          associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          systemProgram: SystemProgram.programId,
+          payerMintAta: payerMintAta,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+          payer: payer.publicKey,
+        })
+        .signers([payer])
+        .rpc();
+
+      console.log("Your transaction signature", tx);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  
 });
 
